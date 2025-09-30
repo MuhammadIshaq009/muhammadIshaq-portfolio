e5ea626656f01674c3726648dc4e6ee0bdacfd03
MuhammadIshaq1998 <mishaqch6620@gmail.com>
2025-09-30 16:31:51 -0400

    added colors

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "Currently holding the position of MERN Stack Developer at Txend Inc since May 2024, and earlier held the position of Associate Node.js Developer at Icommunix Ltd. I hold a degree in Computer Engineering from COMSATS Lahore, with studies in Real Time Embedded System and Digital System Design. I'm problem-solver, and love creating scalable web applications, and I'm now getting deeper into AWS technology. Listed with PEC, Pakistan, and looking forward to continuing to grow in the tech world!",
   email: 'mishaq.connect@gmail.com',
   phone: '+923078700466',
-  address: 'Eden Lane Villas 1, Lahore, Pakistan',
+  address: 'AL Rabaya Building, Al Qasais 2 Dubai, United Arab Emirates',
   github: 'https://github.com/MUHAMMADISHAQ009',
   facebook: '',
   linkedIn: 'https://www.linkedin.com/in/mishaq-connect/',
